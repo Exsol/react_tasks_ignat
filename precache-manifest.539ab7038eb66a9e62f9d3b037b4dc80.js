82bd80eb6b4e6f55ef6491e0c24dd0b035372e53
Vladislav Kislenko <quale161@gmail.com>
2022-05-31 17:29:40 +0300

    Updates

---
+++ precache-manifest.539ab7038eb66a9e62f9d3b037b4dc80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58fc14a4df0f99190040a2b155dc4d5",
+    "url": "/react_tasks_ignat/index.html"
+  },
+  {
+    "revision": "0f7e05a3c7646247783b",
+    "url": "/react_tasks_ignat/static/css/main.6380bb24.chunk.css"
+  },
+  {
+    "revision": "4384277e91a90aed50c3",
+    "url": "/react_tasks_ignat/static/js/2.464289bf.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/react_tasks_ignat/static/js/2.464289bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7e05a3c7646247783b",
+    "url": "/react_tasks_ignat/static/js/main.7e9c4038.chunk.js"
+  },
+  {
+    "revision": "d8ec83b1d760b7aad099",
+    "url": "/react_tasks_ignat/static/js/runtime-main.b5767824.js"
+  }
+]);
